519813afe2686c4ba5c6d4178b4a3d471263c938
Timm Reich <timmreich@hotmail.de>
2022-04-01 21:14:50 +0200

    no : anymore in the timestamp

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import re
 
 delay = 35
 
+input_file = "readme.txt"
+output_file = "output.txt"
+
 pattern = re.compile("[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}")
 
 from datetime import timedelta
 from random import randrange
 
 print("Start creating:" + str(datetime.now()))
+
+with open(input_file, "w") as file:
+    file.truncate()
 
 for line in range(100000):
     with open('readme.txt',"a") as variable_name:
@@ -36,17 +42,25 @@ def add_delay(timestamp, delay):
         if len(list[len(list)-2]) == 1:
             list[1] = "0" + list[1]
 
+    i = 0
+
     for element in list:
         return_list+= element
-        return_list+= ":"
+        i += 1
+        if i != len(list):
+            return_list+= ":"
 
     return return_list
 
 print("Start:" + str(datetime.now()))
-for i, line in enumerate(open('readme.txt')):
+
+with open(output_file, "w") as file:
+    file.truncate()
+
+for i, line in enumerate(open(input_file)):
     for match in re.finditer(pattern, line):
         # Write the file out again
-        with open('output.txt', 'a') as file:
+        with open(output_file, 'a') as file:
             file.write(line.replace(match.group(), add_delay(match.group(), delay)))
     #if i == 3:
     #    break
